02e4746b7354e15345148e04f13ec06a9d3e2aaf
Meredith Hopkins <twolightsabovethesea@gmail.com>
2021-08-24 10:39:42 -0400

    added print statements for routes and run statement for app

--- Images/climate_app.py
+++ Images/climate_app.py
@@ -10,21 +10,37 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    
+    print("Server recieved request for 'home' page")
 
 #create precitipation route
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
+    print("Server recieved request for 'precipitation' page")
     
 #create stations route
 
 @app.route("/api/v1.0/stations")
 def stations():
+    print("Server recieved request for 'precipitation' page")
 
     
 # create tobs route
 
 @app.route("/api/v1.0/tobs")
 def tobs():
+    print("Server recieved request for 'tobs' page")
+#create start route
+
+@app.route("/api/v1.0/<start>")
+def start():
+    print("Server recieved request for 'start' page")
     
+#create start/end route
+
+@app.route("/api/v1.0/<start>/<end>")
+def start_end():
+    print("Server recieved request for 'start_end' page")
+    
+if name == "__main__":
+    app.run(debug=True)
